ff16270a758a9e386d2ae0bba675cb1c8d31a312
Daniel <dnl.crtz46@gmail.com>
2023-09-03 13:10:42 +0800

    Modify reverseString.js and reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+const reverseString = function(inputSting) {
+    const reversedString = inputSting.split("").reverse().join("");
+    return reversedString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
