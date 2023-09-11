ab85c0f468184cb24335fcb11ae13f873e9bd3f6
Daniel <dnl.crtz46@gmail.com>
2023-09-11 20:56:36 +0800

    Modify removeFromArray.js and removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,4 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = (array, ...removeArrayIndex) => array.filter(val => !removeArrayIndex.includes(val));
 
 // Do not edit below this line
 module.exports = removeFromArray;
